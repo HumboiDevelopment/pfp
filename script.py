300f05f83f2e9d0d1bfef556d648251e112d13ed
zendevil <prikshet21@gmail.com>
2022-04-13 14:35:05 -0400

    add sample_props and non-probabalistic generation script

--- script.py
+++ script.py
@@ -1,19 +1,41 @@
 from PIL import Image
 import numpy as np
-
-
+import os
+import random
 
 def combine_props(props):
-    "props' z-index is equal to their position in the props list"
+    """Props' z-index is equal to their position in the props list."""
     final = props[-1]
     for attr in reversed(props):
         final = np.where(attr[:, :, 3:4] == 0, final, attr)
     return final
 
-props = [np.array(Image.open('0.png')),
-         np.array(Image.open('1.png')),
-         np.array(Image.open('2.png'))]
+def sample_props(props_path):
+    props = []
+    props_names = []
+    for path, dirs, files in os.walk(props_path):
+        if(len(files) > 0):
+            filename = random.choice(files)
+            fullpath = os.path.join(path, filename)
+            this_file = np.array(Image.open(fullpath))
+            props_names.append(fullpath)
+            props.append(this_file)
+    return props, props_names
 
-final = combine_props(props)
-img = Image.fromarray(final)
-img.save('final.png')
+
+generated = []
+duplicates = 0
+num_images = 10
+while len(generated) < num_images:
+    props, props_names = sample_props('./properties')
+    new_hash = hash(tuple(props_names))
+    if new_hash not in generated:
+        generated.append(new_hash)
+        image = combine_props(props)
+        image = Image.fromarray(image)
+        image_id = str(len(generated))
+        print(image_id)
+        image.save('./images/' + image_id + '.png')
+    else:
+        duplicates += 1
+print('duplicates: ' + str(duplicates))
